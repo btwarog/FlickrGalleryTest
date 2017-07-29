c2a83998e5bec24e6c1bbc3dbdb6715be11bf795
Radek <radoslaw.juszczyk@amistad.pl>
2017-07-29 23:57:56 +0200

    simple test for json photos response parsing

---
+++ app/src/androidTest/java/com/example/flickrgallery/photosearch/action/DataInputStreamDecoderImplTest.java
@@ -0,0 +1,36 @@
+package com.example.flickrgallery.photosearch.action;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.flickrgallery.screen.photosearch.action.DataInputStreamDecoderImpl;
+import com.example.flickrgallery.screen.photosearch.model.Photo;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.InputStream;
+import java.util.List;
+
+@RunWith(AndroidJUnit4.class)
+public class DataInputStreamDecoderImplTest {
+
+    @Test
+    public void jsonDecodeTest() {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("json_response");
+        DataInputStreamDecoderImpl dataInputStreamDecoder = new DataInputStreamDecoderImpl();
+        boolean isExceptionThrown = false;
+        List<Photo> result = null;
+        try {
+            result = dataInputStreamDecoder.decode(inputStream);
+        } catch (Exception e) {
+            isExceptionThrown = true;
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(isExceptionThrown);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(result.size(), 20);
+    }
+}
